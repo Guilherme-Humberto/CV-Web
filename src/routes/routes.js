131fafcf6774c4072ded72fa13f500a63c986d39
Guilherme Humberto <gbreu@outlook.com>
2020-09-10 22:10:13 -0300

    Configuração das rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Main from '../pages/Main'
 import UserPage from '../pages/UserPage'
 import NotFound from '../pages/NotFound'
+import { isAuthenticated } from '../config/auth'
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route 
+    {...rest}
+    render={props => isAuthenticated() ? (
+      <Component {...props}/>
+    ): (
+      <Redirect to={{ pathname: "/", state: { from: props.location } }}/>
+    )}
+  />
+)
 
 function Routes() {
   return (
       <Switch>
           <Route exact path="/" component={Main}/>
-          <Route path="/Home" component={UserPage}/>
+          <PrivateRoute path="/Home" component={UserPage}/>
           <Route component={NotFound}/>
       </Switch>
   );
