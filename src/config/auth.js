b58a18dc99cef7e290dad98e642965daa44b21aa
Guilherme Humberto <gbreu@outlook.com>
2020-09-23 21:47:19 -0300

    Configuração serviços rest

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,2 +1,7 @@
 // Configuração de autenticação do usuário
-export const isAuthenticated = () => true
+const STORAGE_KEY = ""
+
+export const isAuthenticated = () => !!localStorage.getItem(STORAGE_KEY)
+export const login = (token) => localStorage.setItem(STORAGE_KEY, token)
+export const logout = (token) => localStorage.removeItem(STORAGE_KEY, token)
+export { STORAGE_KEY }
