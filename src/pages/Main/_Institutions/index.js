ece6cf2719e1b1611c4787d22ff0329cc71d9a05
Guilherme-Humberto <gbreu@outlook.com>
2020-10-10 11:12:06 -0300

    Páginas do site

--- src/pages/Main/_Institutions/index.js
+++ src/pages/Main/_Institutions/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './styles.css'
+import React, { useState } from 'react';
 import { BsArrowRight } from 'react-icons/bs'
 
+import './styles.css'
 import imgInts from '../../../assets/imgInst.png'
+import ModalInsts from '../../../components/Modals/ModalInsts'
 
 function Institutions() {
+  const [modalInst, setModalInst] = useState(false)
   return (
     <>
-      <div id="anc-inst" className="container-Institutions">
+        <div id="anc-inst" className="container-Institutions">
           <div id="texts-instituicao">
             <p id="title-insti">Instituições</p>
             <p id="desc-insti">
@@ -15,12 +17,16 @@ function Institutions() {
               junte-se a nós e ajude-nos a levar a doação de sangue a cada vez
               mais áreas.
             </p>
-            <button type="button" id="btnInst">
+            <button onClick={() => setModalInst(true)} type="button" id="btnInst">
               Conheça nossos parceiros <BsArrowRight size={30} color={"darkred"}/>
             </button>
           </div>
           <img id="image-inst" src={imgInts} alt="Imagem Instituição" />
         </div>
+
+        {modalInst && (
+          <ModalInsts buttonclosemodal={() => setModalInst(false)}/>
+        )}
     </>
   );
 }
