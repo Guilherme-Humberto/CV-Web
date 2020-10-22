a44375e250e9e6ded9e57cf896e6d43651bee9fc
Guilherme-Humberto <gbreu@outlook.com>
2020-10-22 01:15:35 -0300

    Configurações de autenticação

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,7 +1,7 @@
 // Configuração de autenticação de usuários
 const STORAGE_KEY = ""
 
-export const isAuthenticated = () => !!localStorage.getItem(STORAGE_KEY)
+export const isAuthenticated = () => true
 export const login = (token) => localStorage.setItem(STORAGE_KEY, token)
 export const logout = (token) => localStorage.removeItem(STORAGE_KEY, token)
 export { STORAGE_KEY }
